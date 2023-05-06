e5a32161287302e0344e2139eadeaf5329d6930b
Justyna Konikiewicz <zagorskaj@wp.pl>
2023-05-06 19:57:34 +0200

    add css styles, add map from leaflet

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-alert("działąm")
+'use strict'
+
+// const loadMap = function () {
+
+// }
+
+// loadMap()
+
+const map = L.map('map').setView([51.505, -0.09], 13)
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	maxZoom: 19,
+	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+}).addTo(map)
+
+const marker = L.marker([51.5, -0.09]).addTo(map)
